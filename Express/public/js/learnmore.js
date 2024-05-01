dcfe996fce997db952e06031b835fa85aa56b4db
Suraj Vinod Miskin <Surajvmiskin@gmail.com>
2024-05-01 13:32:49 +0530

    Delete Express directory

--- Express/public/js/learnmore.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function() {
-//   $("#learnMoreButton").click(function() {
-//     $('html, body').animate({
-//       scrollTop: $("#info").offset().top
-//     }, 500);
-//   });
-// });
